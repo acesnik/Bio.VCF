d533e3dee582a1a6aeb8e7c22b0cc2765b5bb432
Anthony <anthony.cesnik@gmail.com>
2017-10-17 11:52:35 -0500

    added a test

---
+++ TestVCF/VCFUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace VCFComparison
+{
+    [TestFixture]
+    public class VCFUnitTests
+    {
+        [Test]
+        public void test()
+        {
+
+        }
+    }
+}
